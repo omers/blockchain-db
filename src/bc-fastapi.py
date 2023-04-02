ed0312f2cc95528474618348eda73d55d8b618f9
Omer Segev <omersegev@gmail.com>
2023-04-02 14:59:06 +0300

    Refactored to use fastapi

---
+++ src/bc-fastapi.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+"""
+	blockchain_db_server.py - BlockchainDB Server
+	Author: Hoanh An (hoanhan@bennington.edu)
+	Date: 12/5/2017
+"""
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from flask import Flask, jsonify, render_template
+from uuid import uuid4
+from random import randint
+import random
+
+from blockchain_db import BlockchainDB
+
+app = Flask(__name__)
+app = FastAPI()
+blockchain_db_manager = BlockchainDB()
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/', response_class=HTMLResponse)
+async def hello_world(request: Request):
+    """
+    Welcome to Blockchain message
+    :return: HTML
+    """
+    response = {
+        'header': 'Welcome to BlockchainDB'
+    }    
+    return templates.TemplateResponse("landing.html", {"request": request, "data": response})
+
+@app.get('/reset', response_class=HTMLResponse)
+async def reset(request: Request):
+    """
+    Drop the database and start all over again by creating the genesis block.
+    Run once when start, or whenever you feel like dropping!
+    :return: HTML
+    """
+    blockchain_db_manager.reset()
+    response = {
+        'header': 'Successfully generated a genesis block'
+    }
+    return templates.TemplateResponse("landing.html", {"request": request, "data": response})
+
+@app.get('/mine/{number}', response_class=HTMLResponse)
+def mine_blocks(request: Request, number: int):
+    """
+    Mine for a some number of blocks with random generated transactions.
+    :return: HTML
+    Here we can get the payload and mine it
+    {"id": <ID>, "vote_id": <number>}
+    """
+    transactions_range = randint(1, 10)
+    pm = ['bibi','benet','gantz']
+    for i in range(number):
+        for transaction in range(transactions_range):
+            blockchain_db_manager.add_transaction(sender=(str(uuid4()).replace('-', '')[:-10]),
+                                          recipient=pm[random.randint(0,2)],
+                                          amount=1)
+        blockchain_db_manager.mine_for_next_block()
+
+    response = {
+        'header': 'Successfully mined {0} blocks'.format(number)
+    }
+
+    return templates.TemplateResponse("landing.html", {"request": request, "data": response})
+
+@app.get('/view/chain', response_class=HTMLResponse)
+def view_blockchain(request: Request):
+    """
+    View the full BlockChain.
+    :return: HTML
+    """
+    response = {
+        'chain': blockchain_db_manager.get_all_blocks(),
+        'length': blockchain_db_manager.get_length(),
+        'header': 'Full chain'
+    }
+    return templates.TemplateResponse("chain.html", {"request": request, "data": response})
+
+@app.get('/view/last_blocks/{number}', response_class=HTMLResponse)
+def view_last_n_block(request: Request, number: int):
+    """
+    View the last number of mined blocks.
+    :param number: Number of blocks
+    :return: HTML
+    """
+    # Reverse order to display latest ones to oldest one
+    temp = []
+    blocks = blockchain_db_manager.get_last_n_blocks(number)
+    for i in range(number - 1, -1, -1):
+        
+        temp.append(blocks[i])
+
+    response = {
+        'chain': temp,
+        'length': number,
+        'header': 'Last {0} Blocks'.format(number)
+    }
+    return templates.TemplateResponse("chain.html", {"request": request, "data": response})
+
+@app.get('/view/last_block', response_class=HTMLResponse)
+def view_last_block(request: Request):
+    """
+    View the last block.
+    :return: HTML
+    """
+    response = {
+        'chain': [blockchain_db_manager.get_last_block()],
+        'length': 1,
+        'header': 'Last Block'
+    }
+    return templates.TemplateResponse("chain.html", {"request": request, "data": response})
+
+@app.get('/view/genesis_block', response_class=HTMLResponse)
+def view_genesis_block(request: Request):
+    """
+    View the genesis block.
+    :return: HTML
+    """
+    response = {
+        'chain': [blockchain_db_manager.get_genesis_block()],
+        'length': 1,
+        'header': 'Genesis Block'
+    }
+    return templates.TemplateResponse("chain.html", {"request": request, "data": response})
+
+@app.get('/view/block/{number}', response_class=HTMLResponse)
+def view_block(request: Request, number: int):
+    """
+    View a specific block for a given height number.
+    :param number: Block height
+    :return: HTML
+    """
+    response = {
+        'chain': [blockchain_db_manager.get_block(number)],
+        'length': 1,
+        'header': 'Block {0}'.format(number)
+    }
+    return templates.TemplateResponse("chain.html", {"request": request, "data": response})
+
+@app.get('/view/top/{number}/{state}}', response_class=HTMLResponse)
+def view_top_blocks(request: Request, number: int, state: str):
+    """
+    View a number of top blocks for a given state.
+    :param number: Number of blocks
+    :param state: difficulty | elapsed_time | block_reward | hash_power | height | nonce | number_of_transaction
+    :return: HTML
+    """
+    # Reverse order to display latest ones to oldest one
+    temp = []
+    blocks = blockchain_db_manager.get_top_blocks(state=state, number=number)
+    for i in range(number - 1, -1, -1):
+        temp.append(blocks[i])
+
+    response = {
+        'chain': temp,
+        'length': number,
+        'header': 'Top {0} {1}'.format(number, state)
+    }
+    return templates.TemplateResponse("chain.html", {"request": request, "data": response})
